d6938e035fbc9ba3d245e11e177fefb354896c58
Michael Kevin <michaelkmb@hotmail.com>
2023-03-31 11:49:43 -0300

    banner component structure;

---
+++ movies-app/src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import styles from "./Banner.module.css"
+
+function Banner() {
+    return (
+    <section className={ styles.banner__sect } >
+        <span>Teste banner</span>
+    </section>
+    );
+}
+
+export default Banner;
