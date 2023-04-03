1f1484dbf6c9965f45834f271f76caa031011917
Michael Kevin <michaelkmb@hotmail.com>
2023-04-03 11:23:54 -0300

    remove deprecated; minor visual change;

--- movies-app/src/components/Banner/index.js
+++ movies-app/src/components/Banner/index.js
@@ -3,7 +3,6 @@ import styles from "./Banner.module.css"
 function Banner() {
     return (
     <section className={ styles.banner__sect } >
-        <span>Teste banner</span>
     </section>
     );
 }
