d8cc3ed9ca20352ee6c725c51c7c74a74b048e05
Michael Kevin <michaelkmb@hotmail.com>
2023-04-12 15:46:06 -0300

    dynamic categories;

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -4,11 +4,6 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Category from "./components/Category";
-import Card from "./components/Card";
-
-import reactjs from "./json/reactjs.json";
-import nodejs from "./json/nodejs.json";
-import mysql from "./json/mysql.json";
 
 function App() {
   return (
@@ -16,22 +11,7 @@ function App() {
       <Header />
       <Banner bannerImg="home" />
       <Container>
-
-        <h2 className="cat__title">ReactJS</h2>
-        <Category>
-          { reactjs.map( (video) => <Card videoId={video.id} key={video.id} />) }
-        </Category>
-
-        <h2 className="cat__title">NodeJS</h2>
-        <Category>
-          { nodejs.map( (video) => <Card videoId={video.id} key={video.id} />) }
-        </Category>
-
-        <h2 className="cat__title">MySQL</h2>
-        <Category>
-          { mysql.map( (video) => <Card videoId={video.id} key={video.id} />) }
-        </Category>
-
+        <Category />
       </ Container>
 
       <Footer />
--- movies-app/src/components/Category/index.js
+++ movies-app/src/components/Category/index.js
@@ -1,11 +1,35 @@
 import styles from "./Category.module.css"
+import Card from "../Card";
+import videos from "../../json/videos.json";
 
-function Category( {children} ) {
+function Category() {
+
+    let catVideos = {}, uniqueCat = [];
+    videos.map( (video) => {
+        (catVideos[video.category] === undefined) ?
+            catVideos[video.category] = [video] :
+            catVideos[video.category] = [...catVideos[video.category], video];
+        
+        if(!uniqueCat.includes(video.category))
+            uniqueCat = [...uniqueCat, video.category];
+    })
+
     return (
         <section className={ styles.cat__sect }>
-            {children}
+                {
+                uniqueCat.map( (cat) => {
+                    return(
+                        <article key={ cat }>
+                            <h2 className={ styles.cat__title }>{ cat }</h2>
+                            <section className={ styles.videos__sect }>
+                                { catVideos[cat].map( (video) => { return <Card videoId={ video.id } key={ video.id } /> } ) }
+                            </section>
+                        </ article>
+                    )
+                })
+                }
         </section>
-    );
+    )
 }
 
 export default Category;
