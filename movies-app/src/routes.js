4bb1dfffafc6548644a9d453dd21397810c760af
Michael Kevin <michaelkmb@hotmail.com>
2023-07-15 20:59:05 -0300

    ~ move: Page errorNotFound from components to pages;

--- movies-app/src/routes.js
+++ movies-app/src/routes.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import ErrorNotFound from "./components/ErrorNotFound";
+import ErrorNotFound from "./pages/ErrorNotFound";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
