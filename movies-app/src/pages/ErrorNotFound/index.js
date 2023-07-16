fe32871bdea00737d35316e460e087053ffadacc
Michael Kevin <michaelkmb@hotmail.com>
2023-07-15 21:27:55 -0300

    + Error404 page prototp;

--- movies-app/src/pages/ErrorNotFound/index.js
+++ movies-app/src/pages/ErrorNotFound/index.js
@@ -1,9 +1,20 @@
 import styles from "./ErrorNotFound.module.css";
+import error404Img from "../../imgs/erro404.png";
+// TODO: WHY the img can't be on public/imgs as others?!
+// issue: investigate import differences!
 
 function ErrorNotFound() {
     return (
         <main className={ styles.content }>
-        Erro404
+        
+        <img src={ error404Img }
+            alt="Logo do MinstrelFlix gigante com um óculos representando o ato de procurar algo inexistente"
+            className={ styles.errorImg }>
+        </img>
+        <section className={ styles.errorTextColumn }>
+            <h1>Erro 404</h1>
+            <h2>Oops! Não encontramos a página requisitada. Tem certeza que ela existe?</h2>
+        </section>
         </main>
     );
 }
