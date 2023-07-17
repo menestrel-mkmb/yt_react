3556b12c3e930e901f179d70517b26a02d1a2ef5
Michael Kevin <michaelkmb@hotmail.com>
2023-07-16 21:55:24 -0300

    ~ fix: Watch page after reset css;

--- movies-app/src/pages/Watch/index.js
+++ movies-app/src/pages/Watch/index.js
@@ -5,10 +5,12 @@ function Watch({ videoId }){
     return(
         <main className={ styles.content__main }>
             <h1 className={ styles.watch__title }>Assistir</h1>
-            <iframe width="854" height="480"
-            src={ youtubeStructure.linkEmbed + videoId}
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div className={ styles.yt__iframe }>
+                <iframe width="854" height="480"
+                src={ youtubeStructure.linkEmbed + videoId}
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
             <a
                 href={ youtubeStructure.videoLink + videoId }
                 rel="noreferrer noopener"
