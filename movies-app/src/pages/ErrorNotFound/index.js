13c16713b66aeb233d6a44304bd8ee393c9ec461
Michael Kevin <michaelkmb@hotmail.com>
2023-07-16 21:33:43 -0300

    ~ fix: visual fix for empty space;

--- movies-app/src/pages/ErrorNotFound/index.js
+++ movies-app/src/pages/ErrorNotFound/index.js
@@ -6,15 +6,14 @@ import error404Img from "../../imgs/erro404.png";
 function ErrorNotFound() {
     return (
         <main className={ styles.content }>
-        
-        <img src={ error404Img }
-            alt="Logo do MinstrelFlix gigante com um óculos representando o ato de procurar algo inexistente"
-            className={ styles.errorImg }>
-        </img>
-        <section className={ styles.errorTextColumn }>
-            <h1>Erro 404</h1>
-            <h2>Oops! Não encontramos a página requisitada. Tem certeza que ela existe?</h2>
-        </section>
+            <img src={ error404Img }
+                alt="Logo do MinstrelFlix gigante com um óculos representando o ato de procurar algo inexistente"
+                className={ styles.errorImg }>
+            </img>
+            <section className={ styles.errorTextColumn }>
+                <h1>Erro 404</h1>
+                <h2>Oops! Não encontramos a página requisitada. Tem certeza que ela existe?</h2>
+            </section>
         </main>
     );
 }
